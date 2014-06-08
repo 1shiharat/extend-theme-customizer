d29c684147d325c9b020e1d198e6797eae20d8a4
ishihara_takashi <ishihara_note02@ishihara-note02.local>
2014-06-08 16:50:21 +0900

    add xample.json

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -3,8 +3,9 @@
 <p>json ファイルからテーマカスタマイザーの設定を行うことができます。</p>
 
 <form action="" method="POST">
-  <input type="text" name="etc_json_file" rows="30" cols="110"><?php echo $option; ?><br>
+  <p> json ファイルのURL を入力してください。( <a href="https://gist.github.com/">Gist</a> なども使用できます。 )</p>
+  <input type="text" name="etc_json_file" rows="30" value="<?php echo $option; ?>" class="regular-text"><br>
   <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
   <input type="hidden" name="_wp_nonce" value="<?php echo esc_attr( $create_nonce );?>" class="wp_nonce" />
-  <input type="submit" class="button button-primary">
+  <input type="submit" class="button button-primary" value="<?php echo __( '送信', 'extend-theme-customizer' ); ?>">
 </form>
