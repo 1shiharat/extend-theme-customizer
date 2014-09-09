4ff0b04ba855b9e5ebc46ae2c428903fde1c1afc
1shiharaT <akeome1369@gmail.com>
2014-09-09 20:14:39 +0900

    Addeed setting create page.

--- admin/extend-theme-customizer-admin.php
+++ admin/extend-theme-customizer-admin.php
@@ -34,16 +34,21 @@ class ETC_Admin {
 
 		$plugin = ETC_Theme_Customizer::get_instance();
 		$this->plugin_slug = $plugin->get_plugin_slug();
+		$plugin_basename = plugin_basename( plugin_dir_path( realpath( dirname( __FILE__ ) ) ) . $this->plugin_slug . '.php' );
 
-		add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
+		add_action( 'admin_menu', array( $this, 'add_plugin_admin_submenu' ) );
+
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
-		$plugin_basename = plugin_basename( plugin_dir_path( realpath( dirname( __FILE__ ) ) ) . $this->plugin_slug . '.php' );
+
+		/**
+		 * Register Post Type
+		 */
+		add_action( 'init', array( $this, 'register_post_type' ) );
 		add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
-
-		add_action( 'admin_init', array( $this, 'etc_admin_update_option' ) );
-		add_action( 'admin_init', array( $this, 'etc_admin_add_option' ) );
-
+		add_action( 'admin_init', array( $this, 'admin_update_option' ) );
+		add_action( 'admin_init', array( $this, 'admin_add_option' ) );
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
 	}
 
 	/**
@@ -75,7 +80,7 @@ class ETC_Admin {
 
 		if ( $this->plugin_screen_hook_suffix == $screen->id ) {
 			// wp_enqueue_style( $this->plugin_slug . '-admin-styles', plugins_url( 'assets/css/admin.css', __FILE__ ), array(), ETC_Admin::VERSION );
-			wp_enqueue_style( $this->plugin_slug . '-admin-styles-json-editor', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css', array(), ETC_Admin::VERSION );
+			// wp_enqueue_style( $this->plugin_slug . '-admin-styles-json-editor', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css', array(), ETC_Admin::VERSION );
 		}
 
 	}
@@ -101,22 +106,23 @@ class ETC_Admin {
 
 	}
 
-	/**
-	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
-	 *
-	 * @since    1.0.0
-	 */
-	public function add_plugin_admin_menu() {
+	public function add_plugin_admin_submenu() {
 
-		$this->plugin_screen_hook_suffix = add_options_page(
+		$this->plugin_screen_hook_suffix = add_submenu_page(
+			'edit.php?post_type=etc-settings',
 			__( 'Extends Theme Customizer', $this->plugin_slug ),
 			__( 'ETC Settings', $this->plugin_slug ),
 			'manage_options',
-			$this->plugin_slug,
+			'admin.php?page=extend-theme-customizer',
 			array( $this, 'display_plugin_admin_page' )
 		);
 
 	}
+
+	public function display_plugin_admin_addnew_page(){
+		include 'views/add-new.php';
+	}
+
 
 	/**
 	 * Render the settings page for this plugin.
@@ -158,7 +164,7 @@ class ETC_Admin {
 	 *
 	 * @return boolean
 	 */
-	public function etc_admin_update_option()
+	public function admin_update_option()
 	{
 
 		$json_file_path = $_POST['etc_json_file'];
@@ -180,7 +186,7 @@ class ETC_Admin {
 	 *
 	 * @return boolean
 	 */
-	public function etc_admin_add_option()
+	public function admin_add_option()
 	{
 
 		$success = false;
@@ -198,5 +204,74 @@ class ETC_Admin {
 		}
 		return $success;
 
+	}
+
+	/**
+	* Registers a new post type
+	* @uses $wp_post_types Inserts new post type object into the list
+	*
+	* @param string  Post type key, must not exceed 20 characters
+	* @param array|string  See optional args description above.
+	* @return object|WP_Error the registered post type object, or an error object
+	*/
+	public function register_post_type(){
+
+		$labels = array(
+			'name'                => __( 'ETC Settings', $this->plugin_slug ),
+			'singular_name'       => __( 'ETC Setting', $this->plugin_slug ),
+			'add_new'             => _x( 'Add ETC Setting', $this->plugin_slug, $this->plugin_slug ),
+			'add_new_item'        => __( 'Add ETC Setting', $this->plugin_slug ),
+			'edit_item'           => __( 'Edit ETC Setting', $this->plugin_slug ),
+			'new_item'            => __( 'New ETC Setting', $this->plugin_slug ),
+			'view_item'           => __( 'View ETC Setting', $this->plugin_slug ),
+			'search_items'        => __( 'Search ETC Settings', $this->plugin_slug ),
+			'not_found'           => __( 'No ETC Setting found', $this->plugin_slug ),
+			'not_found_in_trash'  => __( 'No ETC Setting found in Trash', $this->plugin_slug ),
+			'parent_item_colon'   => __( 'Parent ETC Setting:', $this->plugin_slug ),
+			'menu_name'           => __( 'Setting Lists', $this->plugin_slug ),
+		);
+
+		$args = array(
+			'labels'               => $labels,
+			'hierarchical'        => false,
+			'description'         => 'description',
+			'public'              => false,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'show_in_admin_bar'   => false,
+			'menu_position'       => 100,
+			'menu_icon'           => null,
+			'show_in_nav_menus'   => true,
+			'publicly_queryable'  => false,
+			'exclude_from_search' => false,
+			'has_archive'         => false,
+			'query_var'           => false,
+			'can_export'          => true,
+			'rewrite'             => false,
+			// 'capability_type'     => '',
+			'supports'            => array(
+				'title',
+				)
+		);
+
+		register_post_type( 'etc-settings', $args );
+	}
+
+	public function add_meta_box( $post_type ){
+		$post_types = array( 'etc-settings' );     //limit meta box to certain post types
+		if ( in_array( $post_type, $post_types ) ) {
+			add_meta_box(
+				'etc_settings_meta_box'
+				,__( 'Option Create', 'extend-theme-customizer' )
+				,array( $this, 'post_meta_render' )
+				,$post_type
+				,'advanced'
+				,'high'
+			);
+    }
+	}
+
+	public function post_meta_render(){
+		include 'views/metaboxes.php';
 	}
 }
